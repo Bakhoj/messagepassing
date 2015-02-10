28b8df98eac482b9e08ae7839a560b3ae57bd37e
Lars <larspeter94@gmail.com>
2015-02-10 11:38:15 +0100

    randomGenerator made

--- MessagePassing/src/client/TemperatureGenerator.java
+++ MessagePassing/src/client/TemperatureGenerator.java
@@ -1,5 +1,28 @@
 package client;
 
+import java.util.*;
+
+// used http://www.javapractices.com/topic/TopicAction.do?Id=62 but changed to our use
+
 public class TemperatureGenerator {
+	final int RANGE_START = 14;
+	final int RANGE_END	= 24;
+	int tempGen;
+	
+	public int temperatureGen(){
+		Random randomGenerator = new Random();
+		getRandomInt(RANGE_START, RANGE_END, randomGenerator);
+		System.out.println(tempGen);
+		return tempGen;
+	}
+	
+	private void getRandomInt(int start, int end, Random random){
+		if (start > end){
+			throw new IllegalArgumentException("start is greater than end");
+		}
+		int range = end - start + 1;
+		double fraction = range * random.nextDouble();
+		tempGen = (int)fraction + start;
+	}
 
 }

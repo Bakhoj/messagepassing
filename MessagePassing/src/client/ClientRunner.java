f02af6d816d68ced626a63d0c5c024cc37927c74
Lars <larspeter94@gmail.com>
2015-02-17 11:23:15 +0100

    DET JO IKKE C?!??!

---
+++ MessagePassing/src/client/ClientRunner.java
@@ -0,0 +1,15 @@
+package client;
+
+import java.io.*;
+import java.net.*;
+import client.client;
+import client.TemperatureGenerator;
+
+
+public class ClientRunner {
+	client client = new client();
+	
+	public void run() {
+		
+	}
+}

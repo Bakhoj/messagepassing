afb7ff83c656bc350dba24f811499f9bef3471b5
anders <anders@Bk-Studie>
2015-02-17 12:47:54 +0100

    wait what ?

--- MessagePassing/src/server/ServerRunner.java
+++ MessagePassing/src/server/ServerRunner.java
@@ -1,11 +1,15 @@
 package server;
 import java.io.*;
-import java.net.*;
+
 import server.server;
 
 public class ServerRunner {
 	server server = new server();
 	int read = 0;
+	int sum = 0;
+	int amount = 0;
+	double avg = 0;
+	
 	public void run(){
 		server.open_socket();
 		server.input_stream();
@@ -17,17 +21,44 @@ public class ServerRunner {
 			System.out.println(e);
 		}
 		
-		temp_method();
-		
-		server.close_socket();
+		while( available() > 0 || true){
+			temp_method();
+			if(read == -1){
+				server.close_socket();
+				break;
+			}
+			calc_avg();
+		}
 	}
 	
-	public void temp_method(){
+	
+	public int available(){
 		try{
-			read = server.input.readByte();
+			return server.input.available();
 		}
 		catch(IOException e){
 			System.out.println(e);
 		}
+		return 0;
+	}
+	
+	
+	public void temp_method(){
+		try{
+			read = 0;
+			if(available()>0) {read = server.input.readByte();}
+			if(read != -1 || read != 0){
+				sum = sum + read;
+				amount++;	
+			}
+		}
+		catch(IOException e){
+			System.out.println(e);
+		}
+	}
+	
+	public void calc_avg(){
+		avg = (sum/amount);
+		System.out.println("The average temperatur is: " + avg + ".");
 	}
 }

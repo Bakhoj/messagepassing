42feafe2f1e31edbf38196c604f578f72f738665
Lars <larspeter94@gmail.com>
2015-02-19 12:09:14 +0100

    satte nogle syso ind så det gav mening

--- MessagePassing/src/client/ClientRunner.java
+++ MessagePassing/src/client/ClientRunner.java
@@ -12,22 +12,28 @@ public class ClientRunner {
 	Scanner scanner = new Scanner(System.in);
 	String read_from_kb = scanner.nextLine();
 	TemperatureGenerator tempGen = new TemperatureGenerator();
+	int temp;
 	boolean program_start = false;
 	
 	public void run() {
+		
 		if(read_from_kb.equals("s")){
 			client.open_socket();
 			System.out.println("program startet");
 			program_start = true;
 		}
+		
 		if(read_from_kb.equals("") && program_start){
-			client.output.write(tempGen.temperatureGen());
+			temp = tempGen.temperatureGen();
+			client.output.write(temp);
+			System.out.println(temp);
 		}
-		if(read_from_kb.equals("q") && program_start){
+		
+		if(read_from_kb.equals("c") && program_start){
 			client.output.write(-1);
 			client.close_socket();
 			program_start = false;
+			System.out.println("program closed");
 		}
-		
 	}
 }

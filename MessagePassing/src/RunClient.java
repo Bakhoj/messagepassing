e3f2b94e67526445133f5966833e32c10a388e7f
Lars <larspeter94@gmail.com>
2015-02-19 12:03:25 +0100

    I was just standing here and tom cruise locked himself in the closet. And I asked myself, why won't tom cruise come out the closet? Nobody has no answers. And SO I PULL OUT MY GUUN?!! - fiksede lige clientRunner

--- MessagePassing/src/RunClient.java
+++ MessagePassing/src/RunClient.java
@@ -1,7 +1,9 @@
 import java.io.IOException;
+import client.ClientRunner;
 
 public class RunClient {
 	public static void main(String[] args) throws IOException{
-		
+		ClientRunner client = new ClientRunner();
+		client.run();
 	}
 }
--- MessagePassing/src/client/ClientRunner.java
+++ MessagePassing/src/client/ClientRunner.java
@@ -1,6 +1,7 @@
 package client;
 
 import java.io.*;
+import java.util.Scanner;
 import java.net.*;
 import client.client;
 import client.TemperatureGenerator;
@@ -8,8 +9,25 @@ import client.TemperatureGenerator;
 
 public class ClientRunner {
 	client client = new client();
+	Scanner scanner = new Scanner(System.in);
+	String read_from_kb = scanner.nextLine();
+	TemperatureGenerator tempGen = new TemperatureGenerator();
+	boolean program_start = false;
 	
 	public void run() {
+		if(read_from_kb.equals("s")){
+			client.open_socket();
+			System.out.println("program startet");
+			program_start = true;
+		}
+		if(read_from_kb.equals("") && program_start){
+			client.output.write(tempGen.temperatureGen());
+		}
+		if(read_from_kb.equals("q") && program_start){
+			client.output.write(-1);
+			client.close_socket();
+			program_start = false;
+		}
 		
 	}
 }
